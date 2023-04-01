22da17205360f339b9a87f9541c2c508452951b0
Yassine AN <Yassine.anaini@gmail.com>
2023-04-01 12:26:28 +0000

    add navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+import $ from "jquery";
+
+
+$(document).ready(function(){
+    //Toggle for user icon in the nav
+    $(".user-click").click(function(){
+      $("#targetEl").toggle();
+    });
+
+    //Toggle mobile menu 
+    $(".burger-menu").click(function(){
+        $("#mobile-menu").toggle();
+      });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors:{
+      primaryCustom : 'rgb(245 245 245)',
+      purpleCustom :'purple',
+      bgPrimary : 'rgb(250 250 250)'
+    }
   },
   variants: {
   extend: {},
